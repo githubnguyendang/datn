2f59f0fae8154eca7e10850391c73980368a41ef
NT.Dang.20 <73978541+githubnguyendang@users.noreply.github.com>
2024-03-06 16:53:20 +0700

    Update config.js

--- FloodForecast/src/api/config.js
+++ FloodForecast/src/api/config.js
@@ -1,5 +1,5 @@
-// const apiUrl = 'https://flood.tainguyenmoitruongquangngai.vn';
+const apiUrl = 'https://flood.tainguyenmoitruongquangngai.vn';
 
-const apiUrl = 'https://localhost:7161';
+// const apiUrl = 'https://localhost:7161';
 
 export default apiUrl;
